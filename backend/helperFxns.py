7b74316df5ea001aa51f335d2bb295ce295e08d3
Aidan <14150272+Aidan-John@users.noreply.github.com>
2023-03-26 14:41:54 -0400

    database reset logic, start of api

--- backend/helperFxns.py
+++ backend/helperFxns.py
@@ -1,4 +1,6 @@
 import random
+import sqlite3
+
 
 ##### obfuscateLyrics(string, string, string, number between 0 and 1)
 
@@ -10,7 +12,7 @@ import random
 ##### and the resulting obfuscatedLyrics. 
 
 ##### obfuscatedLyrics is an array of lyrics,
-##### obfuscated ones replaced with an underscore (_)
+##### obfuscated ones replaced with an underscore '_'
 
 def obfuscateLyrics(songLyrics, songName, songArtist, percentage):
     obfuscated_lyrics = []
@@ -48,3 +50,71 @@ def obfuscateLyrics(songLyrics, songName, songArtist, percentage):
     }
 
 
+class Lyridactle_DB:
+    def connect(self):
+        try:
+            return sqlite3.connect("data.db")
+        except:
+            print("Something went wrong connecting to DB.")
+
+    def reset(self):
+        try:
+            delete_file = open("data.db", "w")
+            delete_file.close()
+            db = self.connect()
+            create_song_table = """CREATE TABLE songs (
+                songID INT NOT NULL,
+                artist VARCHAR(255) NOT NULL,
+                lyrics VARCHAR(255) NOT NULL,
+                songName VARCHAR(255) NOT NULL,
+                obfPattern VARCHAR(255) NOT NULL
+            );"""
+            create_leaderboard_table = """CREATE TABLE leaderboard (
+                topFive VARCHAR(255)
+            );"""
+            create_user_table = """CREATE TABLE users (
+                cookie VARCHAR(255) NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                levelsUnlocked INT NOT NULL,
+                savePoint VARCHAR(255) NOT NULL
+            );"""
+
+            db.execute(create_song_table)
+            db.execute(create_leaderboard_table)
+            db.execute(create_user_table)
+            db.commit()
+            print("Table reset.")
+            return True
+        except:
+            print("Something went wrong with table reset.")
+            return False
+        
+        finally:
+            db.close()
+    
+    def addUser(self):
+        return None
+
+    def updateUser(self):
+        return None
+    
+    def getUser(self):
+        return None
+    
+    def getLeaderboard(self):
+        return None
+    
+    def addSong(self):
+        return None
+    
+    def updateSong(self):
+        return None
+    
+    def getSong(self):
+        return None
+    
+    def deleteUser(self):
+        return None
+        
+    def getPoints(self):
+        return None
--- backend/test_helperFxns.py
+++ backend/test_helperFxns.py
@@ -1,6 +1,10 @@
 import unittest
 from helperFxns import *
 
+###################### IMPORTANT ######################
+###### RUNNING THIS TEST WILL CLEAR THE DATABASE ######
+#######################################################
+
 
 class TestUtils(unittest.TestCase):
     sample_lyrics = "I used to bite my tongue and hold my breath Scared to rock the boat and make a mess So I sat quietly Agreed politely  I guess that I forgot I had a choice I let you push me past the breaking point I stood for nothing So I fell for everything  You held me down, but I got up (Hey!) Already brushing off the dust You hear my voice, you hear that sound Like thunder gonna shake the ground You held me down, but I got up (Hey!) Get ready 'cause I've had enough I see it all, I see it now  I got the eye of the tiger A fighter Dancing through the fire 'Cause I am a champion and you're gonna hear me roar Louder, louder than a lion 'Cause I am a champion and you're gonna hear me roar  Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh You're gonna hear me roar  Now I'm floating like a butterfly Stinging like a bee I earned my stripes I went from zero To my own hero  You held me down, but I got up (Hey!) Already brushing off the dust You hear my voice, you hear that sound Like thunder gonna shake the ground You held me down, but I got up (Got up!) Get ready 'cause I've had enough I see it all, I see it now  I got the eye of the tiger A fighter Dancing through the fire 'Cause I am a champion and you're gonna hear me roar Louder, louder than a lion 'Cause I am a champion and you're gonna hear me roar  Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh You're gonna hear me roar  Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh You'll hear me roar Oh oh oh oh oh oh oh You're gonna hear me roar  Roar, roar, roar, roar, roar  I got the eye of the tiger A fighter Dancing through the fire 'Cause I am a champion and you're gonna hear me roar Louder, louder than a lion 'Cause I am a champion and you're gonna hear me roar  Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh You're gonna hear me roar  Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh You'll hear me roar Oh oh oh oh oh oh oh You're gonna hear me roar"
@@ -19,7 +23,18 @@ class TestUtils(unittest.TestCase):
         
         actualObfuscation = count / song_length
         self.assertTrue(abs(percentage - actualObfuscation) < .05)
+
+    def test_database_reset(self):
+        reset_db = Lyridactle_DB().reset()
         
+        conn = sqlite3.connect("data.db").cursor()
+        conn.execute("""SELECT name FROM sqlite_schema WHERE type='table' AND name NOT LIKE 'sqlite_%';""")
+        result = conn.fetchall()
+        self.assertTrue( result[0][0] == "songs")
+        self.assertTrue( result[1][0] == "leaderboard")
+        self.assertTrue( result[2][0] == "users")
+        self.assertTrue( reset_db == True)
+          
         
 
 if __name__ == "__main__":
