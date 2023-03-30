a6484f58ab58919a2388d6535da34652c4f6cc35
Franco Maxey <francomaxey@msn.com>
2023-03-30 00:20:48 -0400

    How-To, Level Popup, Guess title/lyrics fill-in

--- client/script.js
+++ client/script.js
@@ -24,75 +24,79 @@ addGuessBtn.addEventListener('click', function() {
         for (i in finishedSong) {
             if(guessString == finishedSong[i].toLowerCase()){
                 wordCount+=1;
-                // Add code to fill in the brokeSong array and reload it on page
+                // Fill in the brokeSong array
                 brokeSong[i] = finishedSong[i];
             }
         }
-        // Update page
+        // If they guess the song name
+        if(guessString == songName.toLowerCase()){
+            brokeSong = finishedSong; // Fill in the whole lyrics
+            songBlank = songName; // Fill in the song name
+            wordCount+=1
+        }
+        // Reload page with updated solution
         updatePage();
-        console.log(brokeSong); //testing
+        
+        // Update table
+        modifyTable(guessString,wordCount);
+}
 
-        // Create a new row
-        const newRow = document.createElement('tr');
-    
-        // Create three new <td> elements for each column
-        const numberCell = document.createElement('td');
-        const guessCell = document.createElement('td');
-        const hitsCell = document.createElement('td');
-    
-        // Add content to the cells
-        numberCell.textContent = tableBody.children.length + 1; // Increment row number
-        guessCell.textContent = guessString;
-        hitsCell.textContent = String(wordCount);
-    
-        // Append the cells to the new row
-        newRow.appendChild(numberCell);
-        newRow.appendChild(guessCell);
-        newRow.appendChild(hitsCell);
-    
-        // Insert the new row at the top of the table
-        tableBody.insertBefore(newRow, tableBody.firstChild);
-    }
+function modifyTable(guessString,wordCount){
+    // Create a new row
+    const newRow = document.createElement('tr');
+
+    // Create three new <td> elements for each column
+    const numberCell = document.createElement('td');
+    const guessCell = document.createElement('td');
+    const hitsCell = document.createElement('td');
+
+    // Add content to the cells
+    numberCell.textContent = tableBody.children.length + 1; // Increment row number
+    guessCell.textContent = guessString;
+    hitsCell.textContent = String(wordCount);
+
+    // Append the cells to the new row
+    newRow.appendChild(numberCell);
+    newRow.appendChild(guessCell);
+    newRow.appendChild(hitsCell);
+
+    // Insert the new row at the top of the table
+    tableBody.insertBefore(newRow, tableBody.firstChild);
+}
 
     // Clear the input field
     guessInput.value = "";
 });
 
-// Add function to receive lyric data from database
-
-// Data received:
-// Song Name - String
-// Song Artist - String
-// Percentage Difficulty - String
-// Array of strings (completed song)
-// Array of strings (lyrics with obfuscated words)
-
-
-// Load data from database into page (temp data below)
-let songName = "Roar";
-let songArtist = "Katy Perry";
-let percentDif = "20%";
-let finishedSong = ['i', 'used', 'to', 'bite', 'my', 'tongue', 'and', 'hold', 'my', 'breath', 'scared', 'to', 'rock', 'the', 'boat', 'and', 'make', 'a', 'mess', 'so', 'i', 'sat', 'quietly', 'agreed', 'politely', 'i', 'guess', 'that', 'i', 'forgot', 'i', 'had', 'a', 'choice', 'i', 'let', 'you', 'push', 'me', 'past', 'the', 'breaking', 'point', 'i', 'stood', 'for', 'nothing', 'so', 'i', 'fell', 'for', 'everything', '~', 'you', 'held', 'me', 'down,', 'but', 'i', 'got', 'up', '(hey!)', 'already', 'brushing', 'off', 'the', 'dust', 'you', 'hear'];
-let brokeSong = ['_', 'used', 'to', 'bite', 'my', '______', 'and', 'hold', 'my', '______', 'scared', 'to', 'rock', 'the', '___', 'and', 'make', 'a', 'mess', 'so', 'i', 'sat', '_______', 'agreed', 'politely', 'i', 'guess', 'that', 'i', 'forgot', 'i', 'had', 'a', 'choice', 'i', 'let', 'you', 'push', 'me', 'past', 'the', 'breaking', 'point', 'i', 'stood', 'for', 'nothing', 'so', 'i', 'fell', 'for', 'everything', '~', 'you', 'held', 'me', 'down,', 'but', 'i', 'got', 'up', '(hey!)', 'already', 'brushing', 'off', 'the', 'dust', 'you', 'hear'];
-updatePage();
-
-// Update the page whenever a song is loaded in, or the broke song array is modified
+// Update the content whenever a song is loaded in, or a guess was correct
 function updatePage() {
     let title = document.getElementById('name');
     let artist = document.getElementById('artist');
     let lyrics = document.getElementById('lyrics');
-    console.log('hi');
+
     // Clear out the page
     title.innerHTML = "";
     artist.innerHTML = "";
     lyrics.innerHTML = "";
 
-    title.innerHTML += "'";
-    title.innerHTML += songName;
-    title.innerHTML += "'";
+    // Make the song title blank if it hasnt been guessed yet
+    if(songBlank[0] == '_'){
+        title.innerHTML += "'";
+        for(char in songBlank){
+            title.innerHTML += String.fromCharCode(9619);
+        }
+        title.innerHTML += "'";
+    }
+    else{
+        title.innerHTML += "'";
+        title.innerHTML += songBlank; // Fill in title if it was guessed
+        title.innerHTML += "'";
+    }
 
+    // Reveal Song Artist
     artist.innerHTML += songArtist;
 
+    // Format lyrics
     for(i in brokeSong){
         // Add Line Breaks when necessary to separate parts of the song
         if(brokeSong[i] == "~"){
@@ -110,4 +114,80 @@ function updatePage() {
             lyrics.innerHTML += " ";
         }
     }
+    roundWin();
 }
+
+// Check round win at end of guess
+function roundWin(){
+    let finished = true;
+    // Check if title was guessed or if all lyrics were guessed
+    if(songName != songBlank){
+        for(i in finishedSong){
+            if(finishedSong[i] != brokeSong[i]){
+                finished = false;
+            }
+        }
+    }
+
+    if(finished){
+        let title = document.getElementById('name');
+        title.innerHTML = "";
+        title.innerHTML += "'";
+        title.innerHTML += songName; // Fill in title if it was guessed
+        title.innerHTML += "'";
+
+        // Level Ending Message
+        // Modify text based on level variable
+        popup.classList.add("open-popup"); // Load instructions popup
+        overlay.style.display = 'block';
+        let popupHeader = document.getElementById('popup-header');
+        popupHeader.innerHTML = "Congrats you beat the level!";
+        let popupText = document.getElementById('popup-text');
+        popupText.innerHTML = "You did it.";
+        level += 1;
+    }
+}
+
+// Close instructions popup
+function closePopup(){
+    popup.classList.remove("open-popup");
+    overlay.style.display = 'none';
+}
+
+
+// Requests Data from DB and pulls JSON data and formats it
+function requestData(){
+    console.log('hi');
+}
+
+// Data received:
+// Song Name - String
+// Song Artist - String
+// Percentage Difficulty - String
+// Array of strings (completed song)
+// Array of strings (lyrics with obfuscated words)
+
+
+// Load data from database into page (temp data below)
+let songName = "Roar";
+let songBlank = "____";
+let songArtist = "Katy Perry";
+let percentDif = "20%";
+let finishedSong = ['i', 'used', 'to', 'bite', 'my', 'tongue', 'and', 'hold', 'my', 'breath', 'scared', 'to', 'rock', 'the', 'boat', 'and', 'make', 'a', 'mess', 'so', 'i', 'sat', 'quietly', 'agreed', 'politely', 'i', 'guess', 'that', 'i', 'forgot', 'i', 'had', 'a', 'choice', 'i', 'let', 'you', 'push', 'me', 'past', 'the', 'breaking', 'point', 'i', 'stood', 'for', 'nothing', 'so', 'i', 'fell', 'for', 'everything', '~', 'you', 'held', 'me', 'down,', 'but', 'i', 'got', 'up', '(hey!)', 'already', 'brushing', 'off', 'the', 'dust', 'you', 'hear'];
+let brokeSong = ['_', 'used', 'to', 'bite', 'my', '______', 'and', 'hold', 'my', '______', 'scared', 'to', 'rock', 'the', '___', 'and', 'make', 'a', 'mess', 'so', 'i', 'sat', '_______', 'agreed', 'politely', '_', 'guess', 'that', '_', 'forgot', 'i', 'had', 'a', 'choice', '_', 'let', 'you', 'push', 'me', 'past', 'the', '________', 'point', 'i', 'stood', 'for', 'nothing', 'so', 'i', '____', 'for', 'everything', '~', 'you', 'held', 'me', 'down,', 'but', 'i', 'got', 'up', '(hey!)', 'already', 'brushing', 'off', '___', 'dust', 'you', 'hear'];
+
+let songName2 = "Flowers";
+let songArtist2 = "Miley Cyrus";
+let percentDif2 = "20%";
+let finishedSong2 = ['i', 'used', 'to', 'bite', 'my', 'tongue', 'and', 'hold', 'my', 'breath', 'scared', 'to', 'rock', 'the', 'boat', 'and', 'make', 'a', 'mess', 'so', 'i', 'sat', 'quietly', 'agreed', 'politely', 'i', 'guess', 'that', 'i', 'forgot', 'i', 'had', 'a', 'choice', 'i', 'let', 'you', 'push', 'me', 'past', 'the', 'breaking', 'point', 'i', 'stood', 'for', 'nothing', 'so', 'i', 'fell', 'for', 'everything', '~', 'you', 'held', 'me', 'down,', 'but', 'i', 'got', 'up', '(hey!)', 'already', 'brushing', 'off', 'the', 'dust', 'you', 'hear'];
+let brokeSong2 = ['_', 'used', 'to', 'bite', 'my', '______', 'and', 'hold', 'my', '______', 'scared', 'to', 'rock', 'the', '___', 'and', 'make', 'a', 'mess', 'so', 'i', 'sat', '_______', 'agreed', 'politely', '_', 'guess', 'that', '_', 'forgot', 'i', 'had', 'a', 'choice', '_', 'let', 'you', 'push', 'me', 'past', 'the', '________', 'point', 'i', 'stood', 'for', 'nothing', 'so', 'i', '____', 'for', 'everything', '~', 'you', 'held', 'me', 'down,', 'but', 'i', 'got', 'up', '(hey!)', 'already', 'brushing', 'off', '___', 'dust', 'you', 'hear'];
+
+// INITIAL STARTUP THINGS
+updatePage(); // Initial page update to load lyrics
+
+// Load instructions popup and black overlay
+popup.classList.add("open-popup"); 
+const overlay = document.querySelector('.overlay');
+overlay.style.display = 'block';
+
+let level = 1;
