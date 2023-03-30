6b0c3d32b4079cd1da2acf8f33b8d3bd3cb70451
Aidan <14150272+Aidan-John@users.noreply.github.com>
2023-03-30 14:17:57 -0400

    S2-11 Download top 50 songs and their lyrics

--- backend/helperFxns.py
+++ backend/helperFxns.py
@@ -1,61 +1,16 @@
+from email import header
+import json
 import random
 import sqlite3
 import re
+import os
+import requests
+from dotenv import load_dotenv
+import lyricsgenius
 
-##### obfuscateLyrics(string, string, string, number between 0 and 1)
-#####
-##### Takes input song lyrics, artist name, and song name as strings as
-##### well as a number 0 - 1 indicating the percentage of words to be
-##### obfuscated. 
-#####
-##### Returns: Dictionary with songName, songArtist, songLyrics,
-##### percentage obfuscated, and the resulting obfuscatedLyrics.
-#####
-##### obfuscatedLyrics is an array of lyrics,
-##### obfuscated ones replaced with an underscore '_'
 
-def obfuscateLyrics(songLyrics, songName, songArtist, percentage):
-    obfuscated_lyrics = []
-    lyric_array = songLyrics.lower().split()
-    name_array = songName.lower().split()
-    artist_array = songArtist.lower().split()
-    words_not_allowed = name_array + artist_array
-    song_length = len(lyric_array)
-    obfuscation_indexes = []
 
-    counter = 0
-
-    for lyric in lyric_array:
-        if (re.search("\[[\w]*\]", lyric) != None):
-            obfuscation_indexes.append(counter)
-            counter += 1
-            obfuscated_lyrics.append(lyric)
-        elif lyric in words_not_allowed:
-            obfuscation_indexes.append(counter)
-            counter += 1
-            size = len(lyric)
-            obfuscated_lyrics.append(("_"*size))
-        else:
-            obfuscated_lyrics.append(lyric)
-            counter += 1
-    
-    plain_text_lyrics = set(range(song_length)) - set(obfuscation_indexes)
-    remove = (percentage * song_length) - len(obfuscation_indexes)
-
-    for x in range(round(remove)):
-        plain_lyrics = list(plain_text_lyrics)
-        random_index = random.choice(plain_lyrics)     
-        size = len(lyric_array[random_index])
-        obfuscated_lyrics[random_index] = ("_"*size)
-        plain_text_lyrics.remove(random_index)
-
-    return {
-        "songName": songName,
-        "songArtist": songArtist,
-        "songLyrics": songLyrics,
-        "obfuscatedLyrics": obfuscated_lyrics,
-        "percentage": percentage
-    }
+load_dotenv()
 
 class SavePoint:
     def __init__(self, wordsUsed, id, currentLevel):
@@ -86,12 +41,12 @@ class User:
 
         
 class Song:
-    def __init__(self, songID, artist, lyrics, name, obfPattern):
+    def __init__(self, songID, artist, lyrics, name, obfEasy, obfMedium, obfHard):
         self.id = songID
         self.artist = artist
         self.lyrics = lyrics
         self.name = name
-        self.obfPattern = obfPattern
+        self.obfPatterns = {"easy": obfEasy, "medium": obfMedium, "hard": obfHard}
 
     def json(self):
         return ({
@@ -99,7 +54,7 @@ class Song:
             "artist": self.artist,
             "name": self.name,
             "lyrics": self.lyrics,
-            "obfPattern": self.obfPattern
+            "obfPatterns": self.obfPatterns
         })
 
 
@@ -175,3 +130,130 @@ class Lyridact_DB:
         
     def getPoints(self):
         return None
+
+##### obfuscateLyrics(string, string, string, number between 0 and 1)
+#####
+##### Takes input song lyrics, artist name, and song name as strings as
+##### well as a number 0 - 1 indicating the percentage of words to be
+##### obfuscated. 
+#####
+##### Returns: Dictionary with songName, songArtist, songLyrics,
+##### percentage obfuscated, and the resulting obfuscatedLyrics.
+#####
+##### obfuscatedLyrics is an array of lyrics,
+##### obfuscated ones replaced with an underscore '_'
+
+def obfuscateLyrics(songLyrics, songName, songArtist, percentage):
+    obfuscated_lyrics = []
+    lyric_array = songLyrics.lower().split()
+    name_array = songName.lower().split()
+    artist_array = songArtist.lower().split()
+    words_not_allowed = name_array + artist_array
+    song_length = len(lyric_array)
+    obfuscation_indexes = []
+
+    counter = 0
+
+    for lyric in lyric_array:
+        if (re.search("\[[\w]*\]", lyric) != None):
+            obfuscation_indexes.append(counter)
+            counter += 1
+            obfuscated_lyrics.append("~")
+        elif lyric in words_not_allowed:
+            obfuscation_indexes.append(counter)
+            counter += 1
+            size = len(lyric)
+            obfuscated_lyrics.append(("_"*size))
+        else:
+            obfuscated_lyrics.append(lyric)
+            counter += 1
+    
+    plain_text_lyrics = set(range(song_length)) - set(obfuscation_indexes)
+    remove = (percentage * song_length) - len(obfuscation_indexes)
+
+    for x in range(round(remove)):
+        plain_lyrics = list(plain_text_lyrics)
+        random_index = random.choice(plain_lyrics)     
+        size = len(lyric_array[random_index])
+        obfuscated_lyrics[random_index] = ("_"*size)
+        plain_text_lyrics.remove(random_index)
+
+    return obfuscated_lyrics
+
+
+
+def create_song(songLyrics, songName, songArtist, songID):
+    easy = obfuscateLyrics(songLyrics, songName, songArtist, .2)
+    medium = obfuscateLyrics(songLyrics, songName, songArtist, .5)
+    hard = obfuscateLyrics(songLyrics, songName, songArtist, .7)
+    return(Song(songID, songArtist, songLyrics, songName, easy, medium, hard))
+
+def getSpotifyAccessToken():
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        'Content-Type': "application/x-www-form-urlencoded"
+    }
+    data = {
+        "grant_type": "client_credentials",
+        "client_id": os.environ.get('SPOTIFY_CLIENT_ID'),
+        "client_secret": os.environ.get('SPOTIFY_SECRET'),
+    }
+    res = requests.post(url, headers=headers, data=data)    
+
+    if res.status_code == 200:
+        
+        return res.json()["access_token"]
+    else:
+        return None
+
+def getTop50():
+    topFifty = []
+    playlist_id = "37i9dQZEVXbLp5XoPON0wI"
+    url = "https://api.spotify.com/v1/playlists/" + playlist_id
+    access_token = getSpotifyAccessToken()
+    if access_token == None:
+        return None
+    headers = {
+        'Authorization': "Bearer " + access_token, 
+    }
+    res = requests.get(url, headers=headers)
+    if res.status_code == 200:
+        data = res.json()["tracks"]["items"]
+        for item in data:
+            singers = []
+            artists = item["track"]["artists"]
+            for artist in artists:
+                singerName = artist["name"]
+                singers.append(singerName)
+            name = item["track"]["name"]
+            topFifty.append((name, singers))
+
+        
+        return topFifty
+    else:
+        return None
+
+
+def getLyrics(songName, songArtists):
+    regex = r'[^a-zA-Z0-9\s]'
+    name = re.sub(regex, '', songName.lower())
+    artist = re.sub(regex, '', songArtists[0].lower())
+    query = f'{name} {artist}'
+    url = f"https://api.genius.com/search?q=" + re.sub(" ", "%20", query)
+   
+    access_token = os.environ.get("GENIUS_API_KEY")
+    headers = {
+        'Authorization': "Bearer " + access_token 
+    }
+    res = requests.get(url, headers=headers)
+    data = json.loads(res.text)
+    song_id = data["response"]["hits"][0]["result"]["id"]
+    genius = lyricsgenius.Genius(access_token)
+    lyrics = genius.lyrics(int(song_id))
+    start = lyrics.find('[')
+    clean_lyrics = lyrics[start:-7]
+
+
+    return clean_lyrics
+    
+
--- backend/test_helperFxns.py
+++ backend/test_helperFxns.py
@@ -3,7 +3,7 @@ from helperFxns import *
 
 
 class TestUtils(unittest.TestCase):
-    sample_lyrics = "I used to bite my tongue and hold my breath Scared to rock the boat and make a mess So I sat quietly Agreed politely  I guess that I forgot I had a choice I let you push me past the breaking point I stood for nothing So I fell for everything  You held me down, but I got up (Hey!) Already brushing off the dust You hear my voice, you hear that sound Like thunder gonna shake the ground You held me down, but I got up (Hey!) Get ready 'cause I've had enough I see it all, I see it now  I got the eye of the tiger A fighter Dancing through the fire 'Cause I am a champion and you're gonna hear me roar Louder, louder than a lion 'Cause I am a champion and you're gonna hear me roar  Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh You're gonna hear me roar  Now I'm floating like a butterfly Stinging like a bee I earned my stripes I went from zero To my own hero  You held me down, but I got up (Hey!) Already brushing off the dust You hear my voice, you hear that sound Like thunder gonna shake the ground You held me down, but I got up (Got up!) Get ready 'cause I've had enough I see it all, I see it now  I got the eye of the tiger A fighter Dancing through the fire 'Cause I am a champion and you're gonna hear me roar Louder, louder than a lion 'Cause I am a champion and you're gonna hear me roar  Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh You're gonna hear me roar  Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh You'll hear me roar Oh oh oh oh oh oh oh You're gonna hear me roar  Roar, roar, roar, roar, roar  I got the eye of the tiger A fighter Dancing through the fire 'Cause I am a champion and you're gonna hear me roar Louder, louder than a lion 'Cause I am a champion and you're gonna hear me roar  Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh You're gonna hear me roar  Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh You'll hear me roar Oh oh oh oh oh oh oh You're gonna hear me roar"
+    sample_lyrics = "I used to bite my tounge and hold my breath Scared to rock the boat and make a mess So I sat quietly Agreed politely  I guess that I forgot I had a choice I let you push me past the breaking point I stood for nothing So I fell for everything  You held me down, but I got up (Hey!) Already brushing off the dust You hear my voice, you hear that sound Like thunder gonna shake the ground You held me down, but I got up (Hey!) Get ready 'cause I've had enough I see it all, I see it now  I got the eye of the tiger A fighter Dancing through the fire 'Cause I am a champion and you're gonna hear me roar Louder, louder than a lion 'Cause I am a champion and you're gonna hear me roar  Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh You're gonna hear me roar  Now I'm floating like a butterfly Stinging like a bee I earned my stripes I went from zero To my own hero  You held me down, but I got up (Hey!) Already brushing off the dust You hear my voice, you hear that sound Like thunder gonna shake the ground You held me down, but I got up (Got up!) Get ready 'cause I've had enough I see it all, I see it now  I got the eye of the tiger A fighter Dancing through the fire 'Cause I am a champion and you're gonna hear me roar Louder, louder than a lion 'Cause I am a champion and you're gonna hear me roar  Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh You're gonna hear me roar  Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh You'll hear me roar Oh oh oh oh oh oh oh You're gonna hear me roar  Roar, roar, roar, roar, roar  I got the eye of the tiger A fighter Dancing through the fire 'Cause I am a champion and you're gonna hear me roar Louder, louder than a lion 'Cause I am a champion and you're gonna hear me roar  Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh You're gonna hear me roar  Oh oh oh oh oh oh oh Oh oh oh oh oh oh oh You'll hear me roar Oh oh oh oh oh oh oh You're gonna hear me roar"
     sample_artist = "Katy Perry"
     sample_name = "Roar"
 
@@ -13,12 +13,12 @@ class TestUtils(unittest.TestCase):
         result = obfuscateLyrics(self.sample_lyrics, self.sample_artist, self.sample_name, percentage)
 
         count = 0
-        for lyric in result["obfuscatedLyrics"]:
+        for lyric in result:
             if "_" in lyric:
                 count += 1
         
         actualObfuscation = count / song_length
-        print(result)
+        
         self.assertTrue(abs(percentage - actualObfuscation) < .05)
 
     def test_database_reset(self):
@@ -32,6 +32,13 @@ class TestUtils(unittest.TestCase):
         self.assertTrue( result[1][0] == "leaderboard")
         self.assertTrue( result[2][0] == "users")
         self.assertTrue( check == True )
+
+    def test_top50download(self):
+        songs = getTop50()
+        for song in songs:
+            lyrics = getLyrics(song[0], song[1])
+            print(lyrics)
+            break
           
         
 
