7524a51a0a99d4a85f07ef72fb04315cd3f59939
Aidan <14150272+Aidan-John@users.noreply.github.com>
2023-04-03 16:23:42 -0400

    FS reorg

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response, render_template, request
 from helperFxns import *
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="./client")
 database = Lyridact_DB("data.db")
 
 
@@ -16,6 +16,10 @@ def api_downloadSongs():
 @app.route('/api/getDailySongs')
 def api_getSongs():
     return(jsonify(database.sendTodaySongs()))
+
+@app.route('/')
+def homePage():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
