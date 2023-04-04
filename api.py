1a6cc3156be824ca010bbfa888f164afd08475a8
Aidan <14150272+Aidan-John@users.noreply.github.com>
2023-04-03 20:02:21 -0400

    S2-25 S2-26 setup cookies and store guessed words in db

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, make_response, render_template, request
 from helperFxns import *
 
+
 app = Flask(__name__)
 database = Lyridact_DB("data.db")
 
@@ -17,9 +18,40 @@ def api_downloadSongs():
 def api_getSongs():
     return(jsonify(database.sendTodaySongs()))
 
-@app.route('/')
+@app.route('/api/updateUser', methods=['POST'])
+def api_updateUser():
+    cookie = request.cookies.get('cookie')
+    content = request.get_json()
+    wordList = content['words']
+    level = content['level']
+    database.updateUser(cookie, wordList, level)
+    return ('', 204)
+    
+
+@app.route('/', methods=['GET'])
 def homePage():
-    return render_template("index.html")
+
+    cookie = request.cookies.get('cookie')
+    user = database.getUserFromCookie(cookie)
+    if not user:
+        resp = make_response(render_template("index.html", wordlist=[], level=1))
+        newCookie = generateCookie()
+        resp.set_cookie('cookie', newCookie)
+        database.addNewUser(newCookie)
+        return resp
+    else:
+        print(user)
+        person = json.loads(user[0][0])
+        return render_template("index.html", wordlist=person['wordsUsed'], currentLevel=person['levelsUnlocked'])
+
+        
+   
+
+        
+
+
+
+    
 
 
 if __name__ == "__main__":
